066a2b49da972855d58f3eeb9e797633f7287231
vfrancoil <vfrancoil@hotmail.fr>
2022-08-23 16:15:48 +0200

    add dto annonce

---
+++ src/main/java/fr/project/picom/dto/AnnonceDto.java
@@ -0,0 +1,67 @@
+package fr.project.picom.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AnnonceDto {
+	@NonNull
+	@NotNull(message = "Erreur date de création.")
+	private LocalDateTime dateHeureCreation;
+	
+	@NonNull
+	@NotNull(message = "Veuillez renseigner la date de début.")
+	private LocalDateTime dateHeureDebut;
+	
+	@NonNull
+	@NotBlank(message = "Veuillez renseigner le contenu de l'annonce.")
+	private String contenu;
+
+	@NonNull
+	@NotBlank(message = "Veuillez renseigner le numéro de carte bancaire.")
+	private String numeroCarte;
+
+	@NonNull
+	@NotNull(message = "Veuillez renseigner l'année d'expiration de la carte bancaire.")
+	private Integer anneeExpiration;
+	
+	@NonNull
+	@NotNull(message = "Veuillez renseigner le mois d'expiration de la carte bancaire.")
+	private Byte moisExpiration;
+
+	@NonNull
+	@NotBlank(message = "Veuillez renseigner le CVV de la carte bancaire.")
+	private String cryptogramme;
+	
+	@NonNull
+	@NotBlank(message = "Veuillez renseigner le montant.")
+	private Double montantRegleEnEuros;
+	
+	@NonNull
+	@NotBlank(message = "Veuillez renseigner le client.")
+	@ManyToOne
+	private Long idClient;
+	
+	@NonNull
+	@NotBlank(message = "Veuillez renseigner les tranches horaires.")
+	@ManyToMany
+	private List<Long> tranchesHoraires;
+	
+	@NonNull
+	@NotBlank(message = "Veuillez renseigner les zones.")
+	@ManyToMany
+	private List<Long> zones;
+}
