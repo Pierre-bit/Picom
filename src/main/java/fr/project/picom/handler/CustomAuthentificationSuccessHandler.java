7dc4dfca1b7bde73e5e48d1f276342987e7f9d9e
vfrancoil <vfrancoil@hotmail.fr>
2022-08-26 12:24:32 +0200

    add return authenticate user in success handler

--- src/main/java/fr/project/picom/handler/CustomAuthentificationSuccessHandler.java
+++ src/main/java/fr/project/picom/handler/CustomAuthentificationSuccessHandler.java
@@ -1,6 +1,9 @@
 package fr.project.picom.handler;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,12 +13,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class CustomAuthentificationSuccessHandler implements AuthenticationSuccessHandler {
 
+	ObjectMapper om = new ObjectMapper();
+	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 		response.setStatus(HttpStatus.ACCEPTED.value());
+		Map<String, Object> hashMap = new HashMap<>();
+		hashMap.put("user", authentication.getPrincipal());
+		response.getOutputStream().println(om.writeValueAsString(hashMap));
 	}
 
 }
